aceed3c87abaaf076725c966f3c01d833b8e98f3
agatha.bento <agatha.bento@b2wdigital.com>
2022-11-01 12:16:38 -0300

    novo projeto

---
+++ src/test/java/puc/droneentrega/DroneentregaApplicationTests.java
@@ -0,0 +1,13 @@
+package puc.droneentrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneentregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
